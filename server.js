36e2131a4bc8fcc90583aac45dbdb6554e04bd91
Anant Awasthy <anant.awasthy29@gmail.com>
2023-03-24 18:25:43 -0700

    init the base for the project

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const cors = require('cors')
+const app = express();
+var bodyParser = require('body-parser')
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public/build"));
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 5010;
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT}`);
+});
