e187a547c64e613c06417fe41921cbc798fcf69a
Anant Awasthy <anant.awasthy29@gmail.com>
2023-03-28 11:53:00 -0700

    completed the 404 page UI and finished all the stories

--- client/src/Pages/NoMatch/index.js
+++ client/src/Pages/NoMatch/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Img404 from '../../Images/404-error.png'
+import {Container404 , Img404Conatainer} from './style'
 
 function NoMatch() {
   return (
-    <div>NoMatch</div>
+    <Container404>
+      <Img404Conatainer src={Img404}/>
+      <label>Page not found!</label>
+    </Container404>
   )
 }
 
